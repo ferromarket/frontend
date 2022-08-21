0cb0c9ff19e94e6f44d094538a4b8d3b81adc888
Chris Cromer <chris@cromer.cl>
2022-08-20 22:40:24 -0400

    agregar el mostrar ferreteria

---
+++ src/components/ferreteria/ShowFerreteria.vue
@@ -0,0 +1,86 @@
+<template>
+    <CardPanel class="flex justify-content-center">
+        <template #title>
+            <div class="flex justify-content-center flex-wrap card-container">
+                <h2>{{ ferreteria.Nombre }}</h2>
+            </div>
+        </template>
+        <template #content>
+            <div class="w-full justify-content-center p-fluid grid">
+                <div class="field col-12">
+                    {{ ferreteria.Descripcion }}
+                </div>
+                <div class="field col-12">
+                    <strong>Dirección:</strong> {{ ferreteria.Direccion }}, {{ ferreteria.Comuna.Nombre }}, {{ ferreteria.Comuna.Ciudad.Nombre }}, {{ ferreteria.Comuna.Ciudad.Region.Nombre }}
+                </div>
+                <div class="col-12">
+                    <strong>Horario:</strong>
+                </div>
+                <div class="col-12">
+                    <table>
+                        <tr v-for="horario in ferreteria.Horarios" :key="horario.ID">
+                            <td>
+                                &emsp; <strong>{{ horario.Dia.Nombre }}</strong>:
+                            </td>
+                            <td>
+                                &ensp; {{ horario.Abrir.Hora }} a {{horario.Cerrar.Hora}}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </template>
+    </CardPanel>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+
+export default {
+    setup() {
+        onMounted(() => {
+            getFerreteria();
+        });
+
+        const route = useRoute();
+
+        // si el puerto es 8080, no es con proxy
+        const url = new URL(window.location.href);
+        const api = (url.port == "8080") ? "http://localhost:3001" : "/api";
+
+        const ferreteria = ref({
+            Nombre: "",
+            Descripcion: "",
+            Direccion: "",
+            Comuna: {
+                Nombre: "",
+                Ciudad: {
+                    Nombre: "",
+                    Region: {
+                        Nombre: ""
+                    }
+                }
+            },
+            Horarios: []
+        });
+
+        const getFerreteria = () => {
+            axios
+                .get(api + "/ferreteria/" + route.params.id)
+                .then((response) => {
+                    ferreteria.value = response.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        };
+
+        return {
+            ferreteria,
+            getFerreteria
+        };
+    }
+};
+</script>
---
+++ src/views/ferreteria/ShowFerreteria.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="sm:col-4 sm:col-offset-4">
+        <ShowFerreteria/>
+    </div>
+</template>
+
+<script>
+import ShowFerreteria from '../../components/ferreteria/ShowFerreteria.vue'
+
+export default {
+    name: 'App',
+    components: {
+        ShowFerreteria
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+    line-height: 1.5;
+    margin: 0;
+}
+</style>
