aedd33fbe9fbe9c31fea7122eb114a1346b1d2fc
Cristianlr95 <cristia.lagos199507@gmail.com>
2022-08-22 22:07:47 -0400

    Creacion vista del perfil del repartidor

---
+++ src/views/repartidor/PerfilRepartidor.vue
@@ -0,0 +1,20 @@
+<template #content>
+    <PerfilRepartidor />
+</template>
+<script>
+import PerfilRepartidor from "../../components/repartidor/PerfilRepartidor.vue";
+
+export default {
+    name: "App",
+    components: {
+        PerfilRepartidor,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+    line-height: 1.5;
+    margin: 0;
+}
+</style>
